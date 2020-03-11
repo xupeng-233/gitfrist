267fc464870550b618ad066aa5a71653431f4335
xupeg <819995441@qq.com>
2020-03-11 18:38:38 +0800

    branch2

---
+++ HellowWord.java
@@ -0,0 +1,16 @@
+/**
+ * ClassName:HellowWord
+ * Package:PACKAGE_NAME
+ * Description:
+ *
+ * @Date:2020/3/11 17:15
+ * Author:
+ */
+public class HellowWord {
+    public static void main(String[] args) {
+        System.out.println("a");
+        System.out.println("b");
+        System.out.println("c");
+        System.out.println("d");
+    }
+}
