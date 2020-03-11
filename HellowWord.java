dcc9cd5d275322a0d37185e01ee289be9d1bc131
xupeg <819995441@qq.com>
2020-03-11 17:21:35 +0800

    helloword

---
+++ HellowWord.java
@@ -0,0 +1,16 @@
+/**
+ * ClassName:HellowWord
+ * Package:PACKAGE_NAME
+ * Description:
+ *
+ * @Date:2020/3/11 17:13
+ * Author:
+ */
+public class HellowWord {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+    }
+}
